1f270097f3bf1b19d545d1ce407b9ffb13678a06
ImmortalReaper <Artem172902@gmail.com>
2025-06-19 22:54:11 +0300

    add controller presets

---
+++ Assets/Scripts/Feature/ControllerPresets/Scripts/ControllerPreset.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Controller Preset", menuName = "UI/Controller/New Controller Preset")]
+public class ControllerPreset : ScriptableObject
+{
+    [Header("Settings")]
+    public string controllerName = "Controller Name";
+
+    [Space(10)]
+    public List<ControllerItem> items = new List<ControllerItem>();
+
+    public enum ItemType { Icon, Text }
+
+    [System.Serializable]
+    public class ControllerItem
+    {
+        public string itemID;
+        public ItemType itemType;
+        public Sprite itemIcon;
+        public string itemText;
+    }
+}
