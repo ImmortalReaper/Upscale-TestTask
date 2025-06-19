aa052d12ea1437434726d1e3227bc79bc6f4d3b0
ImmortalReaper <Artem172902@gmail.com>
2025-06-19 22:53:52 +0300

    add Adress for addressables

---
+++ Assets/Scripts/AddressablesAddress/Address.cs
@@ -0,0 +1,12 @@
+public static class Address
+{
+    public static class UI
+    {
+        public const string MainMenu = "MainMenu";
+        public const string TitleScreen = "TitleScreen";
+        public const string Settings = "Settings";
+        public const string LoadGame = "LoadGame";
+        public const string Credits = "Credits";
+        public const string ScreenTransition = "ScreenTransition";
+    }
+}
