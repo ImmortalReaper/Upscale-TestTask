f829c8155b12db8fbc004323c9a047aa44267e23
ImmortalReaper <Artem172902@gmail.com>
2025-06-21 01:32:52 +0300

    Update InputService.cs

--- Assets/Scripts/Core/Input/InputService.cs
+++ Assets/Scripts/Core/Input/InputService.cs
@@ -1,6 +1,8 @@
 using System;
+using UnityEngine;
 using UnityEngine.InputSystem;
 using Zenject;
+using Object = UnityEngine.Object;
 
 namespace Core.Input
 {
@@ -8,7 +10,9 @@ namespace Core.Input
     {
         private readonly InputActionAsset _inputActions;
         private UIInputService _uiInputService;
+        private PlayerInput _playerInput;
         
+        public PlayerInput PlayerInput => _playerInput;
         public IUIInputService UIInputService => _uiInputService;
         public InputService(InputActionAsset inputActionAsset)
         {
@@ -17,6 +21,7 @@ namespace Core.Input
 
         public void Initialize()
         {
+            CreatePlayerInput();
             _inputActions.Enable();
             _uiInputService = new UIInputService(_inputActions);
             _uiInputService.Initialize();
@@ -26,6 +31,19 @@ namespace Core.Input
         {
             _inputActions.Disable();
             _uiInputService?.Dispose();
+            Object.Destroy(_playerInput.gameObject);
+        }
+
+        private void CreatePlayerInput()
+        {
+            GameObject playerInputGO = new GameObject("PlayerInput");
+            Object.DontDestroyOnLoad(playerInputGO);
+            
+            _playerInput = playerInputGO.AddComponent<PlayerInput>();
+            
+            _playerInput.actions = _inputActions;
+            _playerInput.defaultControlScheme = "Keyboard & Mouse";
+            _playerInput.neverAutoSwitchControlSchemes = false;
         }
     }
 }
