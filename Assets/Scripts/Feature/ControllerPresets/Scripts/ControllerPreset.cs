22c228c66dcb7b11f4960691b5922832b79437c8
ImmortalReaper <Artem172902@gmail.com>
2025-06-21 01:30:24 +0300

    add controller presets

--- Assets/Scripts/Feature/ControllerPresets/Scripts/ControllerPreset.cs
+++ Assets/Scripts/Feature/ControllerPresets/Scripts/ControllerPreset.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Controller Preset", menuName = "UI/Controller/New Controller Preset")]
 public class ControllerPreset : ScriptableObject
 {
-    [Header("Settings")]
-    public string controllerName = "Controller Name";
-
-    [Space(10)]
-    public List<ControllerItem> items = new List<ControllerItem>();
+    public List<ControllerItem> items = new();
 
     public enum ItemType { Icon, Text }
 
     [System.Serializable]
     public class ControllerItem
     {
-        public string itemID;
+        public HotKeyType hotKeyType;
         public ItemType itemType;
         public Sprite itemIcon;
         public string itemText;
+    }
+    
+    public enum HotKeyType
+    {
+        Exit,
+        GoBack,
+        Navigate,
+        Select,
+        Next,
+        Previous,
+        NextAlt,
+        PreviousAlt
     }
 }
