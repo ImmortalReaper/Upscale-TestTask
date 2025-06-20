7f38cd5736bb67066e4f0f1d85b9c6d30cd3853d
ImmortalReaper <Artem172902@gmail.com>
2025-06-21 01:32:15 +0300

    Update UIModuleInstaller.cs

--- Assets/Scripts/Feature/UIModule/Scripts/UIModuleInstaller.cs
+++ Assets/Scripts/Feature/UIModule/Scripts/UIModuleInstaller.cs
@@ -13,6 +13,8 @@ namespace Feature.UIModule.Scripts
                     { typeof(TitleScreenUI), new UIConfig(Address.UI.TitleScreen, 0, UIWindowType.Normal) },
                     { typeof(MainMenuUI), new UIConfig(Address.UI.MainMenu, 1, UIWindowType.Normal) },
                     { typeof(SettingsUI), new UIConfig(Address.UI.Settings, 2, UIWindowType.Normal) },
+                    { typeof(CreditsUI), new UIConfig(Address.UI.Credits, 3, UIWindowType.Normal) },
+                    { typeof(LoadGameUI), new UIConfig(Address.UI.LoadGame, 4, UIWindowType.Modal) },
                     { typeof(ScreenTransitionUI), new UIConfig(Address.UI.ScreenTransition, 999, UIWindowType.Popup) }
                 });
         }
