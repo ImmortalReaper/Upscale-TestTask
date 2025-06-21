88e5c5c563b61d81a04a4a3d3864f29e280edadf
ImmortalReaper <Artem172902@gmail.com>
2025-06-21 15:33:48 +0300

    Update MainMenuBootstrap.cs

--- Assets/Scripts/Feature/UIModule/Scripts/Menus/MainMenuBootstrap.cs
+++ Assets/Scripts/Feature/UIModule/Scripts/Menus/MainMenuBootstrap.cs
@@ -1,15 +1,33 @@
+using System;
 using UnityEngine;
 using Zenject;
 
 public class MainMenuBootstrap : MonoBehaviour
 {
     private MainMenuStateMachine _mainMenuStateMachine;
+    private IInputDeviceService _inputDeviceService;
     
     [Inject]
-    public void InjectDependencies(MainMenuStateMachine mainMenuStateMachine)
+    public void InjectDependencies(MainMenuStateMachine mainMenuStateMachine, IInputDeviceService inputDeviceService)
     {
+        _inputDeviceService = inputDeviceService;
         _mainMenuStateMachine = mainMenuStateMachine;
     }
+    
+    private async void Start()
+    {
+        await _mainMenuStateMachine.ChangeState<TitleScreenStateUI>();
+        _inputDeviceService.OnDeviceChanged += HandleInputDeviceChanged;
+        Debug.Log(_inputDeviceService.CurrentDevice);
+    }
 
-    private async void Start() => await _mainMenuStateMachine.ChangeState<TitleScreenStateUI>();
+    private void OnDestroy()
+    {
+        _inputDeviceService.OnDeviceChanged -= HandleInputDeviceChanged;
+    }
+
+    private void HandleInputDeviceChanged(ActiveInputDevice obj)
+    {
+        Debug.Log(_inputDeviceService.CurrentDevice);
+    }
 }
